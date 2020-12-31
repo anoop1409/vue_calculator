127abae8c9bb7415c205deb8745b2e83744f7157
Anoop Nair <myapp00777@gmail.com>
2020-12-31 19:48:07 +0100

    Renamed sample component and removed unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Frame msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import Frame from "./components/Frame.vue";
 
 @Options({
   components: {
-    HelloWorld
+    Frame
   }
 })
 export default class App extends Vue {}
---
+++ src/components/Frame.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Calculator</h1>
+    <div class="frame">
+
+    </div>
+  </div>
+
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class Frame extends Vue {
+  msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+.frame
+  width: 500px
+  height: 600px
+  background-color: brown
+</style>
